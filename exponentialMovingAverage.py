9d6bfc834a8ef06c9b6e8241e80a5de7821b2154
timotewb <45491056+timotewb@users.noreply.github.com>
2019-04-29 21:16:31 +0100

    Create exponentialMovingAverage.py

---
+++ exponentialMovingAverage.py
@@ -0,0 +1,25 @@
+def exponentialMovingAverage(periodEMA, data):
+    
+    #--- import libraries
+    import numpy as np
+    
+    #--- define variables
+    p = periodEMA        # period value form self
+    s = data[:p].sum()/p      # the first simple moving average
+    m = 2/(p+1)               # weighting factor
+
+    #--- define output array
+    out = np.zeros(len(data))
+
+    #--- calculate EMA
+    for i in range(len(data)):
+        #--- where data item is the p'th item, use the SMA
+        if i == p-1:
+            out[i] = s
+        elif i > p-1:
+            #--- the EMA calculation
+            out[i] = ((data[i] - out[i-1]) * m) + out[i-1]
+        elif i < p-1:
+            out[i] = np.nan
+
+    return out
