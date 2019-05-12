24ed6f038a5ab4b1c9a63ced27e5ebf35083ce52
timotewb <45491056+timotewb@users.noreply.github.com>
2019-05-12 20:34:42 +0100

    Update 04. doubleExponentialMovingAverage.py

--- 04. doubleExponentialMovingAverage.py
+++ 04. doubleExponentialMovingAverage.py
@@ -4,28 +4,29 @@ def doubleExponentialMovingAverage(periodDEMA, data):
     
     p = periodDEMA
     s = data[:p].sum()/p      # the first simple moving average
-    m = 2/(p+1)               # weighting factor
+    k = 2/(p+1)               # weighting factor
     d = (2 * p) - 1           # start of the DEMA
 
     out = np.zeros(len(data))
+    out_ee = np.zeros(len(data))
     out_d = np.zeros(len(data))
 
     for i in range(len(data)):
-        #--- where data item is the p'th item use the SMA
+        #--- use SMA for first DEMA value
         if i == p-1:
             out[i] = s
+            out_ee[i] = s
+            out_d[i] = s
         elif i > p-1:
-            out[i] = ((data[i] - out[i-1]) * m) + out[i-1]
+            #--- calculate EMA
+            out[i] = (k * data[i]) + (1 - k) * out[i-1]
+            #--- calcualte ema of ema
+            out_ee[i] = (k * out[i]) + (1 - k) * out_ee[i-1]
+            #--- calculate dema
+            out_d[i] = 2 * out[i] - out_ee[i]
         elif i < p-1:
             out[i] = np.nan
-
-        #--- start calculation of DEMA
-        if i == d-1:
-            out_d[i] = out[(p-1):d].sum()/p
-        elif i > d-1:
-            #--- now calcuate the DEMA 2xEMA - ema(EMA)
-            out_d[i] = (2 * out[i]) - (((out[i] - out_d[i-1]) * m) + out_d[i-1])
-        elif i < d-1:
+            out_ee[i] = np.nan
             out_d[i] = np.nan
             
     return out_d
